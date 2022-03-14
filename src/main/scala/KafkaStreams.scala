e8f0ef48f2a3a2b84c1d1a8e9cda7f5a0bc1763b
Mustapha Abdelaziz Khorsi <khom3101@dinf-d60015-24b.DInf.fsci.usherbrooke.ca>
2022-03-14 16:50:43 -0400

    latest version

--- src/main/scala/KafkaStreams.scala
+++ src/main/scala/KafkaStreams.scala
@@ -3,11 +3,16 @@ import io.circe.parser._
 import io.circe.syntax._
 import io.circe.{Decoder, Encoder}
 import org.apache.kafka.common.serialization.Serde
-import org.apache.kafka.streams.kstream.GlobalKTable
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
+import org.apache.kafka.streams.kstream.{GlobalKTable, JoinWindows}
 import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.streams.scala.kstream.{KStream, KTable}
 import org.apache.kafka.streams.scala.serialization.Serdes
 import org.apache.kafka.streams.scala.ImplicitConversions._
+
+import java.time.Duration
+import java.time.temporal.ChronoUnit
+import java.util.Properties
 object KafkaStreams {
 
   object Domain {
@@ -38,9 +43,10 @@ object KafkaStreams {
       val string = new String(bytes)
       decode[A](string).toOption
     }
-    Serdes.fromFn[A](serializer,deserializer)
+    Serdes.fromFn[A](serializer = serializer,deserializer = deserializer)
 
   }
+  def main(args: Array[String]) : Unit ={
   // topology
   val builder = new StreamsBuilder()
   //KStream
@@ -61,9 +67,32 @@ object KafkaStreams {
   val ordersWithUsersProfiles: KStream[UserId, (Order, Profile)] = usersOrdersStream.join(userProfilesTable) {
     (order,profile) => (order,profile)
   }
+  val discountedOrdersStream = ordersWithUsersProfiles.join(userProfilesGTable)(
+    {case (userId,(order,profile)) => profile},
+    {case ((order,profile),discount)=> order.copy(amount = order.amount - discount.amount)}
+    )
+  //change the identifier
+  val ordersStream: KStream[OrderId, Order] = discountedOrdersStream.selectKey((userId, order) => order.orderId)
+  val paymentsStream: KStream[OrderId, Payment] = builder.stream[OrderId,Payment](Payments)
 
-  builder.build()
-  def main(args: Array[String]) : Unit ={
+  val joinWindow: JoinWindows = JoinWindows.of(Duration.of(5,ChronoUnit.MINUTES))
+  val joinOrdersPayments = (order : Order,payment : Payment) => if (payment.status == "PAID") Option(order) else Option.empty[Order]
+
+  val ordersPaid = ordersStream.join(paymentsStream)(joinOrdersPayments,joinWindow).flatMapValues(maybeorder => maybeorder.toList)
+
+  //send the data to a topic
+  ordersPaid.to(PaidOrders)
+
+  val topology = builder.build()
+
+    val props = new Properties
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG,"orders-app")
+    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092")
+    props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,Serdes.stringSerde.getClass)
+
+    val application = new KafkaStreams(topology,props)
+    application.start()
+
   }
 
 }
